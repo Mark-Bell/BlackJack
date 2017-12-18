09d51a2b4734b4954e6b32f406a63fcc2d42322b
Mark-Bell <G00348970@gmit.ie>
2017-12-18 04:38:34 +0000

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,59 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
 using System;
 using System.Collections.Generic;
-using System.IO;     
+using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;    
-using Windows.Foundation;  
-using Windows.Foundation.Collections;    
-using Windows.UI.Xaml;   
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;  
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.Media.Core;
-using Windows.Media.Playback;
 
-namespace MarkBell_Project
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WeatherPanel
 {
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
     public sealed partial class MainPage : Page
     {
-        string[] deck = new String[52];
-
-
         public MainPage()
         {
             this.InitializeComponent();
-
-            for (int i = 0; i < 13; i++)
-            {
-                deck[i] = ((i + 1) + "D");
-                deck[i + 13] = ((i + 1) + "S");
-                deck[i + 26] = ((i + 1) + "H");
-                deck[i + 39] = ((i + 1) + "C");  
-            }
-                shuffle();
-
-            
-        }
-
-        public void shuffle()
-        {
-            for (int i = 51; i >= 0; i--)
-            {
-    
-                Random j = new Random();
-                string[] shuffledDeck = deck.OrderBy(x => j.Next()).ToArray();
-            }
-        }
-
-        public void deal()
-        {
- 
         }
     }
 }
-
